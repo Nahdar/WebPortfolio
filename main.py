3249d52ddbd811e81b3f020fdb582d089383e7e6
Nahdar <christian02@hotmail.com>
2024-05-14 14:59:35 -0600

    add .jinja ending

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ bootstrap = Bootstrap5(app)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html.jinja')
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html.jinja')
 
 if __name__ == "__main__":
     app.run(debug=True)
